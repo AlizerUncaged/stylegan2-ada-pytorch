e094992eed4ad3fc30ebf3489e1a6de661eb74ed
Alizer <86959368+AlizerUncaged@users.noreply.github.com>
2022-05-13 09:58:09 +0800

    Update training_loop.py

--- training/training_loop.py
+++ training/training_loop.py
@@ -26,6 +26,7 @@ import hashlib
 import legacy
 from metrics import metric_main
 import pytz
+from IPython.display import FileLink
 
 # functions
 def md5(fname):
@@ -359,7 +360,9 @@ def training_loop(
         # Save image snapshot.
         if (rank == 0) and (image_snapshot_ticks is not None) and (done or cur_tick % image_snapshot_ticks == 0):
             images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
-            save_image_grid(images, os.path.join(run_dir, f'fakes{cur_nimg//1000:06d}.png'), drange=[-1,1], grid_size=grid_size)
+            imagePath = os.path.join(run_dir, f'fakes{cur_nimg//1000:06d}.png')
+            save_image_grid(images, imagePath, drange=[-1,1], grid_size=grid_size)
+            FileLink(imagePath)
 
         # Save network snapshot.
         pickleFile = ""
